1041e64daea20339eb3abc8d23b78509b1eed114
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2024-01-16 21:19:58 +0100

    Install Bootstrap 5 with Vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
 
         <title>Laravel</title>
 
+        <!-- Bootstrap -->
+        @vite(['resources/js/app.css', 'resources/js/app.js'])
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
