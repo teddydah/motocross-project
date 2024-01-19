7e1a0e9446dd8f3e05ef7bd72625f907a415a2a1
Teddy Dahyot <61130381+teddydah@users.noreply.github.com>
2024-01-19 15:37:32 +0100

    Created user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            ClubSeeder::class
+            ClubSeeder::class,
+            UserSeeder::class
         ]);
 
         // \App\Models\User::factory(10)->create();
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'lastname' => 'Doe',
+            'firstname' => 'John',
+            'email' => 'john.doe@gmail.com',
+            'password' => '$2y$10$Y5LdAjHNd8AG6FC21GNXCuW38Y1ClEgrcinq2fC0Ib//TZv9ycHYW', // Pa$$w0rd
+            'role' => 'admin',
+            'license_number' => '123456',
+            'phone' => '0612345678',
+            'birth_date' => '1900-01-01',
+            'address' => '123 Fake Street',
+            'zip_code' => '12345',
+            'city' => 'Faker City'
+        ]);
+    }
+}
