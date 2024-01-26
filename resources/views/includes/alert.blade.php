c8849abe66499be8b8f0c16290066e7ebec57127
Teddy Dahyot <teddy.dahyot@hotmail.fr>
2024-01-26 23:32:40 +0100

    Updated alert messages

--- resources/views/includes/alert.blade.php
+++ resources/views/includes/alert.blade.php
@@ -1,11 +1,23 @@
-@if ($message = Session::get('success'))
-    <div class="alert alert-success alert-block d-flex justify-content-between align-items-baseline">
-        <strong>{{ $message }}</strong>
+@if (session('success'))
+    <div
+        class="alert alert-success alert-block container d-flex justify-content-between align-items-baseline text-start">
+        <span>{{ session('success') }}</span>
         <i class="close fa-solid fa-xmark" data-dismiss="alert" title="Fermer la fenêtre"></i>
     </div>
-@elseif ($message = Session::get('error'))
-    <div class="alert alert-danger alert-block d-flex justify-content-between align-items-baseline">
-        <strong>{{ $message }}</strong>
+@elseif (session('errors'))
+    <div
+        class="alert alert-danger alert-block container d-flex justify-content-between align-items-center text-start">
+        <div class="d-flex flex-column">
+            @foreach($errors->all() as $error)
+                <span>{{ $error }}</span>
+            @endforeach
+        </div>
+        <i class="close fa-solid fa-xmark" data-dismiss="alert" title="Fermer la fenêtre"></i>
+    </div>
+@elseif (session('warning'))
+    <div
+        class="alert alert-warning alert-block container d-flex justify-content-between align-items-baseline text-start">
+        <span>{{ session('warning') }}</span>
         <i class="close fa-solid fa-xmark" data-dismiss="alert" title="Fermer la fenêtre"></i>
     </div>
 @endif
