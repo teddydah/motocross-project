d41adc9dcd08a07b2b35462fa061e8a62a6194a1
Teddy Dahyot <61130381+teddydah@users.noreply.github.com>
2024-01-29 13:57:54 +0100

    Created PictureSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ClubSeeder::class,
+            PictureSeeder::class,
             TrainingSeeder::class,
             UserSeeder::class
         ]);
---
+++ database/seeders/PictureSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PictureSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('pictures')->insert([
+            'image' => 'auribail_65b7981d0a597.jpg',
+            'description' => 'Photo de Motocross (Auribail Mx Park)',
+            'club_id' => 1
+        ]);
+
+        DB::table('pictures')->insert([
+            'image' => 'auribail_65b79851aa417.jpg',
+            'description' => 'Photo de Motocross (Auribail Mx Park)',
+            'club_id' => 1
+        ]);
+    }
+}
