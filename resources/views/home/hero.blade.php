a1b50cd5eeb2aebd500f0066cb9e3e00a6d0a09b
Teddy Dahyot <61130381+teddydah@users.noreply.github.com>
2024-02-05 14:29:10 +0100

    Updated hero section

--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -7,32 +7,25 @@
             <div class="carousel-inner" role="listbox">
 
                 <!-- Slide 1 -->
-                <div class="carousel-item active" style="background-image: url(img/slide/auribail.jpg);">
+                <div class="carousel-item slide-1 active">
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <img class="w-25 mb-4" src="{{ url('/') }}/img/logo.png" alt="Logo Auribail Mx Park">
-                            <h2 class="animate__animated animate__fadeInDown">Bienvenue à Auribail Mx Park</h2>
                         </div>
                     </div>
                 </div>
 
                 <!-- Slide 2 -->
-                <div class="carousel-item" style="background-image: url(img/slide/slide-2.jpg);">
+                <div class="carousel-item slide-2">
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <h2 class="animate__animated animate__fadeInDown">Bienvenue à Auribail Mx Park</h2>
-                            <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui
-                                aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem
-                                mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti
-                                vel. Minus et tempore modi architecto.</p>
-                            <a href="#about" class="btn-get-started scrollto animate__animated animate__fadeInUp">Read
-                                More</a>
                         </div>
                     </div>
                 </div>
 
                 <!-- Slide 3 -->
-                <div class="carousel-item" style="background-image: url(img/slide/slide-3.jpg);">
+                <!--<div class="carousel-item" style="background-image: url(img/slide/slide-3.jpg);">
                     <div class="carousel-container">
                         <div class="carousel-content">
                             <h2 class="animate__animated animate__fadeInDown">Bienvenue à Auribail Mx Park</h2>
@@ -44,7 +37,7 @@
                                 More</a>
                         </div>
                     </div>
-                </div>
+                </div>-->
             </div>
             <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon ri-arrow-left-line" aria-hidden="true"></span>
